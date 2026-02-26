982907e5b483806d750c6b79f73a5feaeec550cc
unknown <jimenezmateo607@gmail.com>
2026-02-26 08:02:19 -0500

    Se ha creado 01_saludo_formateado.py el cual pregunta nombre edad y ciudad y da una respuesta en base a eso

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,4 @@
+nombre=input("Hola!, ¿Cómo te llamas?: ")
+edad=int(input("¿Cuantos años tienes?: "))
+ciudad=input("¿En qué ciudad vives?")
+print(f"Hola {nombre}, tienes {edad} años y vives en {ciudad}")
