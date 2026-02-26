ca498789d7af11d5ca0914740891c8bba43636cc
unknown <jimenezmateo607@gmail.com>
2026-02-26 08:18:25 -0500

    Se ha creado 02_par_impar.py el cual pide un número y muestra si es par o impar, luego pide edad y muestra si es mayor o menor de edad

---
+++ 02_par_impar.py
@@ -0,0 +1,11 @@
+número=float(input("Dame un número: "))
+if número%2 == 0:
+    print(f"El número {número} es par")
+else:
+    print(f"El número {número} es impar")
+
+edad=float(input("Dame tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("No eres mayor de edad")
