15e56b97096a768dd3d84b61869bd494e52021ff
unknown <jimenezmateo607@gmail.com>
2026-03-02 12:13:53 -0500

    Mejora al código de 02_par_impar.py para que no se rompa

--- 02_par_impar.py
+++ 02_par_impar.py
@@ -1,11 +1,27 @@
-número=float(input("Dame un número: "))
-if número%2 == 0:
-    print(f"El número {número} es par")
-else:
-    print(f"El número {número} es impar")
+while True:
+    try:
+        número=float(input("Dame un número: "))
+        if número%2 == 0:
+            print(f"El número {número} es par")
+            break
+        else:
+            print(f"El número {número} es impar")
+            break
+    except:
+        print("introduce un número valido por favor.")
+while True:
+    try:
+        edad=float(input("Dame tu edad: "))
+        if edad >= 18:
+            print("Eres mayor de edad")
+            break
+        else:
+            print("No eres mayor de edad")
+            break
+    except:
+        print("Introduce un número válido por favor")
 
-edad=float(input("Dame tu edad: "))
-if edad >= 18:
-    print("Eres mayor de edad")
-else:
-    print("No eres mayor de edad")
+
+
+
+
